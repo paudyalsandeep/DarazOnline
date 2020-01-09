9066079c3fbe786f13006ba641cc231b97048d3a
sandeep <weguruchela@gmail.com>
2020-01-09 14:15:12 +0545

    fragments job

--- app/src/main/java/com/example/darazonline/HomeFragment.java
+++ app/src/main/java/com/example/darazonline/HomeFragment.java
@@ -4,6 +4,7 @@ package com.example.darazonline;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,8 @@ import android.view.ViewGroup;
  */
 public class HomeFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,8 +27,14 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        recyclerView=container.findViewById(R.id.recyclerView);
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
     }
 
 }
--- app/src/main/java/com/example/darazonline/adapter/ProductsAdapter.java
+++ app/src/main/java/com/example/darazonline/adapter/ProductsAdapter.java
@@ -1,13 +1,54 @@
 package com.example.darazonline.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProductsAdapter {
+import com.example.darazonline.R;
+import com.example.darazonline.model.Products;
+
+import java.util.List;
+
+public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ProductsViewHolder> {
+
+    Context mContext;
+    List<Products>productsList;
+
+    public ProductsAdapter(Context mContext, List<Products> productsList) {
+        this.mContext = mContext;
+        this.productsList = productsList;
+    }
+
+
+
+
+    @NonNull
+    @Override
+    public ProductsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.dashboard,parent,false);
+
+        return new ProductsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductsViewHolder holder, int position) {
+        Products products=productsList.get(position);
+        holder.productImage.setImageResource(products.getImageId());
+        holder.tvProductName.setText(products.getName());
+        holder.tvPrice.setText(products.getPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return productsList.size();
+    }
 
     public class ProductsViewHolder extends RecyclerView.ViewHolder {
 
@@ -15,6 +56,12 @@ public class ProductsAdapter {
         TextView tvProductName,tvPrice;
         public ProductsViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            productImage=itemView.findViewById(R.id.imageView);
+            tvProductName=itemView.findViewById(R.id.tvProductName);
+            tvPrice=itemView.findViewById(R.id.tvPrice);
+
+
 
         }
     }
