f4a6c49b28ea42da8dc08d6ccba781764de7ba75
sandeep <weguruchela@gmail.com>
2020-01-10 13:58:28 +0545

    design of login is completed

--- app/src/main/java/com/example/darazonline/AccountFragmane.java
+++ app/src/main/java/com/example/darazonline/AccountFragmane.java
@@ -8,12 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AccountFragmane extends Fragment {
+
+    EditText etPhone,etSMS;
+    Button btnSend,btnLogin;
 
 
     public AccountFragmane() {
@@ -25,7 +30,17 @@ public class AccountFragmane extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_fragmane, container, false);
+        View view= inflater.inflate(R.layout.fragment_account_fragmane, container, false);
+
+        etPhone=view.findViewById(R.id.etPhone);
+        etSMS=view.findViewById(R.id.etSMS);
+        btnSend=view.findViewById(R.id.btnSend);
+        btnLogin=view.findViewById(R.id.btnLogin);
+
+
+
+
+        return view;
     }
 
 }
